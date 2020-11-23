ff6f9b1c30b29872c75dadc35276124b7061a07b
Thomas <tom.bauer95@gmail.com>
2020-11-22 23:18:53 -0600

    added cargo.lock to gitignore

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::io::{self, ErrorKind, Read, Write};
+use std::net::TcpStream;
+use std::sync::mpsc::{self, TryRecvError};
+use std::thread;
+use std::time::Duration;
+
+
 fn main() {
     println!("Hello, world!");
 }
